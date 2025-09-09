5448750274757d5c8fb3c68585823a1a283f26a7
Dat Nguyen <dat.nguyen@threadresearch.com>
2025-09-09 18:25:17 +0700

    update seo

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,36 +2,36 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a qualified mobile developer with over 7 years of professional experience, including 5+ years specializing in React Native (including additional experience in Web development with ReactJS, Expo Web) and 2 years in Android (Java). I bring strong creative and analytical skills, a sharp eye for detail, and proven experience delivering high-quality mobile applications. A collaborative team player with excellent English communication skills (both written and spoken), I am highly adaptable, eager to learn new technologies, and driven by a proactive, can-do mindset",
+		keywords: ["dat.dev", "dat-dev", "dat-dev-portfolio", "Nguyen Tan Dat dev", "Dat Nguyen dev", "Dat Nguyen", "Nguyen Tan Dat"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I am a qualified mobile developer with over 7 years of professional experience, including 5+ years specializing in React Native (including additional experience in Web development with ReactJS, Expo Web) and 2 years in Android (Java). I bring strong creative and analytical skills, a sharp eye for detail, and proven experience delivering high-quality mobile applications. A collaborative team player with excellent English communication skills (both written and spoken), I am highly adaptable, eager to learn new technologies, and driven by a proactive, can-do mindset",
+		keywords: ["dat.dev", "dat-dev", "dat-dev-portfolio", "Nguyen Tan Dat dev", "Dat Nguyen dev", "Dat Nguyen", "Nguyen Tan Dat"],
 	},
 
 	{
 		page: "articles",
 		description:
 			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: [],
 	},
 
 	{
 		page: "projects",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: [],
 	},
 
 	{
 		page: "contact",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: [],
 	},
 ];
 
